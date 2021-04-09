312f67cebb19750d82c67b61ee92f1d79bfd6572
Julio Avila <hellboy2015@gmail.com>
2021-04-09 02:59:57 +0000

    initial commit

---
+++ src/app.py
@@ -0,0 +1,50 @@
+from flask import Flask
+from flask import request
+import json
+import flask
+app = Flask(__name__)
+
+todos = [
+        { "label": "My first task", "done": False }
+]
+
+@app.route('/todos', methods=['GET'])
+def hello_world():
+    json_text = flask.jsonify(todos)
+    return json_text
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.data
+    decoded_object = json.loads(request_body)
+    todos.append(decoded_object)
+    print("Incoming request with the following body", request_body)
+    json_text = flask.jsonify(todos)
+    return json_text
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    print("This is the position to delete: ",position)
+    todos.pop(position)
+    json_text = flask.jsonify(todos)
+    return json_text
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# These two lines should always be at the end of your app.py file.
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
